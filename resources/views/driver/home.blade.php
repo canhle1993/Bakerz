8144bef94e9f754b4ed4db7511e5f41d0847f2e6
canhle1993 <aplevancanh1993@gmail.com>
2024-09-19 21:06:48 +0700

    up

--- resources/views/driver/home.blade.php
+++ resources/views/driver/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Bakerfresh - Cake Shop HTML Template</title>
+    <title>Bakerz Bite</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
