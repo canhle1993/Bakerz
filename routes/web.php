e643efe82a4ac1e4a271c2dae5ded1094f46956e
canhle1993 <aplevancanh1993@gmail.com>
2024-09-24 17:19:22 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -82,23 +82,6 @@ Route::delete('/admin/manage/admin/{id}', [ManageAdminController::class, 'destro
 Route::post('/admin/manage/lower-to-client/{id}', [ManageAdminController::class, 'lowerToClient'])->name('admin.lower_to_client');
 Route::post('/admin/manage/update-to-admin/{id}', [ManageAdminController::class, 'Upgradetoadmin'])->name('admin.update_to_admin');
 
-// // Route quản lý Client
-// use App\Http\Controllers\Admin\ManageClientController;
-// Route::get('/admin/manage/client', [ManageClientController::class, 'index'])->name('manage-client');
-// Route::get('/admin/manage/client/create', [ManageClientController::class, 'create'])->name('client.create');
-// Route::post('/admin/manage/client/store', [ManageClientController::class, 'store'])->name('client.store');
-// Route::get('/admin/manage/client/{id}/edit', [ManageClientController::class, 'edit'])->name('client.edit');
-// Route::post('/admin/manage/client/{id}', [ManageClientController::class, 'update'])->name('client.update');
-// Route::delete('/admin/manage/client/{id}', [ManageClientController::class, 'destroy'])->name('client.destroy');
-
-// // Route quản lý Admin
-// use App\Http\Controllers\Admin\ManageAdminController;
-// Route::get('/admin/manage/admin', [ManageAdminController::class, 'index'])->name('manage-admin');
-// Route::get('/admin/manage/admin/create', [ManageAdminController::class, 'create'])->name('admin.create');
-// Route::post('/admin/manage/admin/store', [ManageAdminController::class, 'store'])->name('admin.store');
-// Route::get('/admin/manage/admin/{id}/edit', [ManageAdminController::class, 'edit'])->name('admin.edit');
-// Route::post('/admin/manage/admin/{id}', [ManageAdminController::class, 'update'])->name('admin.update');
-// Route::delete('/admin/manage/admin/{id}', [ManageAdminController::class, 'destroy'])->name('admin.destroy');
 
 // Route cho client shop product types
 Route::get('/shop_all', [ProductController::class, 'all_product'])->name('shop_all');
